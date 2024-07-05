932612aeaf4b2cfc9c57c20ab7ce259888797350
Yadhu Krishna M J <yadddhu@gmail.com>
2024-07-05 17:33:33 +0530

    Added Port support for Render.com

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT||3000, ()=>{
     console.log(`Server is Running`);
 })
